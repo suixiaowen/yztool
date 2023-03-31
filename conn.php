95e4c18e9a22ad7504cd13a5e3fe6928904bb3a2
suixiaowen <97166612+suixiaowen@users.noreply.github.com>
2023-03-31 10:02:41 +0800

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,19 @@
+<?php
+$host = "127.0.0.1";
+$user = "game";
+$pass = "uu5!^%jg";
+$pvf_control = "1";
+$chongzhipaihang = "1";
+$chongzhi_1 = "1";
+$d_gold_control = "1";
+$ip_reg_control = "1";
+$hang_control = "1";
+$frist_control = "1";
+$shop_control = "1";
+$sign_control = "1";
+$bubble_control = "0";
+$reward_gold = "10";
+$paodian_teme = "1";
+$gold_type = "1";
+$genxin = "1";
+?>
